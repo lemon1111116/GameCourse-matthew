81819825f10146bcd73171fec6a18f00b902ab49
Lemon1111116 <matthewmillar175@gmail.com>
2020-08-14 13:56:57 -0500

    made it so you can only jump when you are on the ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,16 +8,26 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
 
     public int jumpForce;
+
+    public Transform groundPoint;
+
+    public LayerMask groundLayer;
+
+    public bool grounded;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapPoint(groundPoint.position,groundLayer);
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
         {
             anim.SetTrigger("Jump");
             rb.AddForce(Vector2.up * jumpForce);
